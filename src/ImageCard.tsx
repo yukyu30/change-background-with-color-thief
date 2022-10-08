cb75e438f9294ccfa6fba34ad434ab649cc05fa8
yu5 <fukanoyugo+dev@gmail.com>
2022-10-08 17:32:49 +0900

    テストコードを書いた

---
+++ src/ImageCard.tsx
@@ -0,0 +1,36 @@
+import Color from "color-thief-react";
+import { ArrayRGB } from 'color-thief-react/lib/types';
+
+function isWhite(rgb: ArrayRGB){
+  const [r, g, b] = rgb.map((value: any) => value)
+  
+  if ((r * 299 + g * 587 + b * 114)/ 1000 >= 130){
+    return true
+  }
+  return false
+}
+
+const getBgColor=(rgb: ArrayRGB | undefined) =>{
+    if (rgb !== undefined) {
+        return isWhite(rgb) ? 'gray' : 'white'
+      } else {
+        return 'gray'
+    }
+}
+const ImageCard = (props: { imgSrc: string }) => {
+  return (
+    <Color src={props.imgSrc} format="rgbArray">
+        {({ data }) => {
+            const bgColor = getBgColor(data)
+            return(     
+                <div>
+                    <img src={props.imgSrc} style={{ backgroundColor: bgColor }}/>
+                    <div>dominant color: {data?.map((value: any) => <span>{value} </span>)}</div>
+                </div>
+            )
+        }}
+    </Color>
+  );
+}
+
+export default ImageCard;
